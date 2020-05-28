533c8d413ef78bde3f2ad236a2974bee9dce1dd5
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-05-28 16:04:50 -0700

    Some fixes needed for version upgrade.

--- orgInit.sh
+++ orgInit.sh
@@ -1,14 +1,33 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o department.demo
+# Create the demo org
+sfdx force:org:create -f config/project-scratch-def.json --setalias departmental-apps --setdefaultusername
+#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o electron.demo
 
+# Push the metadata into the new scratch org.
 sfdx force:source:push
+
+# Assign the permset to our user.
 sfdx force:user:permset:assign -n electron
+
+# Assign the permset to the Integration user (this will be needed to start the analytics dataflow)
 sfdx shane:user:permset:assign -n analytics -g Integration -l User
 
+# Deploy the metadata for the analytics (this needed to happen AFTER the other meta data was pushed)
 sfdx force:source:deploy -p dataflow
+
+# Start the dataflow for the Analytics.
 sfdx shane:analytics:dataflow:start -n Electron
 
+# Deploy the metadata for the visualizations
+sfdx force:source:deploy -p visualization
+
+# Activate the custom theme.
 sfdx shane:theme:activate -n Electron
-sfdx force:org:open
+
+# Set the default password.
 sfdx shane:user:password:set -g User -l User -p sfdx1234
 
-sfdx force:source:deploy -p visualization
+# Open the demo org.
+sfdx force:org:open
+
+
+
